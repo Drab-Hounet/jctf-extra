6965322f6983cf2213c4c8378bf8eb86d23c4007
drabhounet <drabhounet@gmail.com>
2025-05-30 14:13:01 +0200

    Initial commit (login)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jctf-front-extra';
+  title = 'Inscription adhésion';
+  checked: boolean = false;
+
 }
